32b871cf1217702c7e6dd2f19ad545726819a431
Maks <zheltov2001@gmail.com>
2022-12-07 14:50:21 +0300

    Сделал интерфейс всех основных окон

---
+++ app/src/main/java/com/example/medicinehelper/InterviewResult.java
@@ -0,0 +1,14 @@
+package com.example.medicinehelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterviewResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interview_result);
+    }
+}
